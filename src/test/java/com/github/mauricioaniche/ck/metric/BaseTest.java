2ed8be7895dcf2df98f05dfa69789c7933c17176
VincentDS <de_vinnie111_rulezzz@hotmail.com>
2017-04-14 16:05:11 +0200

    cfgFile returns proper path instead of url encoded path

--- src/test/java/com/github/mauricioaniche/ck/metric/BaseTest.java
+++ src/test/java/com/github/mauricioaniche/ck/metric/BaseTest.java
@@ -2,12 +2,13 @@ package com.github.mauricioaniche.ck.metric;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 abstract class BaseTest {
 
 	protected static String fixturesDir() {
 		try {
-			String cfgFile = new File(BaseTest.class.getResource("/").getPath() + "../../fixtures/").getCanonicalPath();
+			String cfgFile = new File("." + "../../fixtures/").getCanonicalPath();
 			System.out.println(cfgFile);
 			return cfgFile;
 		} catch (IOException e) {

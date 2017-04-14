c564ab3baece3676390c9061246cbd0b55bd4cd7
VincentDS <de_vinnie111_rulezzz@hotmail.com>
2017-04-14 19:56:43 +0200

    remove unused import

--- src/test/java/com/github/mauricioaniche/ck/metric/BaseTest.java
+++ src/test/java/com/github/mauricioaniche/ck/metric/BaseTest.java
@@ -2,13 +2,13 @@ package com.github.mauricioaniche.ck.metric;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 abstract class BaseTest {
 
 	protected static String fixturesDir() {
 		try {
 			String cfgFile = new File("." + "../../fixtures/").getCanonicalPath();
+			//System.out.println(BaseTest.class.getResource("/").getPath());
 			System.out.println(cfgFile);
 			return cfgFile;
 		} catch (IOException e) {
